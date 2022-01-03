4832c01e67190b2367a21230c5036729ddd3682e
Brian Williams <bwilliams7622@gmail.com>
2022-01-02 22:25:11 -0600

    express server created

--- assets/server.js
+++ assets/server.js
@@ -1,1 +1,14 @@
-console.log("start and nodemon commands working...")
+const express = require("express")
+const socket = require("socket.io")
+const myRoutes = require("./routes.js")
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/' , myRoutes)
+
+app.listen(3000 , (error) => {
+    if (!error) {
+	console.log("Server currently running @ http://localhost:3000; press ctrl+c to cancel")
+    }
+})
