84f2fce7bc26b5f812076ab445327bd8e730d2ac
Brian Williams <bwilliams7622@gmail.com>
2022-01-03 20:39:37 -0600

    HMTL and CSS markup edited; slider inputs added

--- assets/arduino.js
+++ assets/arduino.js
@@ -3,6 +3,17 @@ const board = new five.Board()
 
 board.on('ready' , () => {
     console.log('connected to Arduino Board')
+    const base = new five.Servo({
+	pin: 9,
+	range: [0 , 180],
+	startAt: 60
+    })
+
+    const claw = new five.Servo({
+	pin: 11,
+	range: [0 , 90],
+	startAt: 0
+    })
 })
 
 module.exports = board
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
+/* socket.io frontend to backend connection */
 const socket = io.connect("http://localhost:3000")
+
+/* HTML class variables */
+const baseSlider = document.getElementsByClassName('base-slider')[0]
+const shoulderSlider = document.getElementsByClassName('shoulder-slider')[0]
+const elbowSlider = document.getElementsByClassName('elbow-slider')[0]
+const wristSlider= document.getElementsByClassName('wrist-slider')[0]
+const clawSlider = document.getElementsByClassName('claw-slider')[0]
