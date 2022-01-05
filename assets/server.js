85780d11ade9ee5dc08ceb27a53a6149b541531c
Brian Williams <bwilliams7622@gmail.com>
2022-01-04 18:01:40 -0600

    base servo moves on frontend slider input change

--- assets/server.js
+++ assets/server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const socket = require("socket.io")
+const five = require("johnny-five")
 const myRoutes = require("./routes.js")
-const test = require("./arduino.js")
 const app = express()
 
 app.use(express.static('public'))
@@ -16,6 +16,36 @@ const server = app.listen(3000 , (error) => {
 
 const io = socket(server)
 
+/* connecting to our board via johnny-five */
+const board = new five.Board()
+
+/* connecting from the backend to the js frontend file */
 io.on('connection' , (socket) => {
     console.log('we\'ve made a socket connection')
+
+    socket.on("baseSlider" , (data) => {
+	console.log(data)
+
+	/*board.on("ready" , () => {
+	    let base = new five.Servo({
+		pin: 11,
+		range: [0 , 150],
+		startAt: data.baseAngle
+	    })
+	})*/
+    })
+
+    board.on("ready" , function (data)  {
+	console.log("arduino board connected")
+
+	let base = new five.Servo({
+	    pin: 9,
+	    range: [0 , 150],
+	    startAt: 60
+	})
+
+	socket.on('baseSlider' , (data) => {
+	    base.to(data.baseAngle)
+	})
+    })
 })
